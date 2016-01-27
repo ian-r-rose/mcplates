2d5820ce351909e9ca0db3491762f81e07a47c40
Ian Rose <ian.r.rose@gmail.com>
2016-01-27 10:08:22 -0800

    Invert for two euler poles

--- test_invert_euler_pole.py
+++ test_invert_euler_pole.py
@@ -9,31 +9,42 @@ from mcplates import *
 import mcplates.rotations_numpy as rnp
 
 # Generate a synthetic data set
-ages =[0.,20.,30.,60.]
-sigma_ages = [2., 2., 2., 2.]
-lon_lats = [ [300., -30.], [360.,0.], [60.,30.], [110., 0.]]
+#ages =[0.,20.,30.,60.]
+#sigma_ages = [2., 2., 2., 2.]
+#lon_lats = [ [300., -30.], [360.,0.], [60.,30.], [110., 0.]]
+ages =[0.,10.,20.,30.,40]
+sigma_ages = [2., 2., 2., 2., 2.]
+lon_lats = [ [300., -20.], [340.,0.], [0.,30.], [20., 0.], [60.,-20.]]
 
-@theano.compile.ops.as_op(itypes=[tt.dvector, tt.dvector, tt.dscalar, tt.dscalar], otypes=[tt.dvector])
-def generate_pole( start_pole_direction, euler_pole_direction, euler_pole_rate, time ):
-    euler_pole = EulerPole( euler_pole_direction[0], euler_pole_direction[1], euler_pole_rate)
-    start_pole = PaleomagneticPole(start_pole_direction[0], start_pole_direction[1], age=time)
+@theano.compile.ops.as_op(itypes=[tt.dvector, tt.dvector, tt.dscalar, tt.dvector, tt.dscalar, tt.dscalar, tt.dscalar], otypes=[tt.dvector])
+def generate_pole( start, euler_1, rate_1, euler_2, rate_2, switchpoint, time ):
+    euler_pole_1 = EulerPole( euler_1[0], euler_1[1], rate_1)
+    euler_pole_2 = EulerPole( euler_2[0], euler_2[1], rate_2)
+    start_pole = PaleomagneticPole(start[0], start[1], age=time)
 
-    start_pole.rotate( euler_pole, euler_pole.rate*time)
+    if time <= switchpoint:
+        start_pole.rotate( euler_pole_1, euler_pole_1.rate*time)
+    else:
+        start_pole.rotate( euler_pole_1, euler_pole_1.rate*switchpoint)
+        start_pole.rotate( euler_pole_2, euler_pole_2.rate*(time-switchpoint))
     return np.array([start_pole.longitude, start_pole.latitude])
     
 generate_pole.grad = lambda *x: x[0]
 
     
 with pymc3.Model() as model:
-    euler_pole_direction = VonMisesFisher('euler_pole', lon_lat=(0.,0.), kappa=0.00)
-    euler_pole_rate = pymc3.Exponential('rate', 1.) 
-    start_pole_direction = VonMisesFisher('start_pole', lon_lat=lon_lats[0], kappa=kappa_from_two_sigma(10.))
+    euler_1 = VonMisesFisher('euler_1', lon_lat=(0.,0.), kappa=0.00)
+    rate_1 = pymc3.Exponential('rate_1', 1.) 
+    euler_2 = VonMisesFisher('euler_2', lon_lat=(0.,0.), kappa=0.00)
+    rate_2 = pymc3.Exponential('rate_2', 1.) 
+
+    start = VonMisesFisher('start', lon_lat=lon_lats[0], kappa=kappa_from_two_sigma(10.))
+    switchpoint = pymc3.Uniform('switchpoint', ages[0], ages[-1])
 
 
     for i in range(len(ages)):
         time = pymc3.Normal('t'+str(i), mu=ages[i], sd=sigma_ages[i])
-        lon_lat = generate_pole(start_pole_direction, euler_pole_direction, euler_pole_rate, time)
-        print lon_lat
+        lon_lat = generate_pole(start, euler_1, rate_1, euler_2, rate_2, switchpoint, time)
         observed_pole = VonMisesFisher('p'+str(i), lon_lat, kappa = kappa_from_two_sigma(10.), observed=lon_lats[i])
         
 
@@ -44,33 +55,58 @@ with pymc3.Model() as model:
 def run(n):
     with model:
         trace = pymc3.sample(n, step, start=start)
-        euler_directions = trace['euler_pole']
-        start_directions = trace['start_pole']
-        rates = trace['rate']
-        elons = euler_directions[:,0]
-        elats = euler_directions[:,1]
+
+        euler_1_directions = trace['euler_1']
+        e1lons = euler_1_directions[:,0]
+        e1lats = euler_1_directions[:,1]
+        rates_1 = trace['rate_1']
+
+        euler_2_directions = trace['euler_2']
+        e2lons = euler_2_directions[:,0]
+        e2lats = euler_2_directions[:,1]
+        rates_2 = trace['rate_2']
+
+        start_directions = trace['start']
         slons = start_directions[:,0]
         slats = start_directions[:,1]
-#        ax = pymc3.traceplot(trace)
-#        plt.show()
-        ax = plt.axes(projection = ccrs.Robinson())
-        ax.scatter(elons, elats, transform=ccrs.PlateCarree(), edgecolors='none', alpha=0.1)
+
+        switchpoints = trace['switchpoint']
+
+        ax = pymc3.traceplot(trace)
+        plt.show()
+
+        ax = plt.axes(projection = ccrs.Orthographic(0.,30.))
+        ax.scatter(e1lons, e1lats, transform=ccrs.PlateCarree(), color='r', edgecolors='none', alpha=0.1)
+        ax.scatter(e2lons, e2lats, transform=ccrs.PlateCarree(), color='g', edgecolors='none', alpha=0.1)
         ax.gridlines()
         ax.set_global()
 
-        interval = int(len(rates)/1000)
+        interval = int(len(rates_1)/1000)
         age_list = np.linspace(ages[0], ages[-1], 100)
         pathlons = np.empty_like(age_list)
         pathlats = np.empty_like(age_list)
-        for slon, slat, elon, elat, rate in zip(slons[::interval], slats[::interval], elons[::interval],elats[::interval],rates[::interval]):
+        print e1lons
+        print e2lons
+        for slon, slat, elon1, elat1, rate1, elon2, elat2, rate2, switch \
+                     in zip(slons[::interval], slats[::interval], 
+                            e1lons[::interval],e1lats[::interval], rates_1[::interval],
+                            e2lons[::interval],e2lats[::interval], rates_2[::interval],
+                            switchpoints[::interval]):
             initial_pole = rnp.spherical_to_cartesian( slon, slat, 1.0 )
-            euler_pole = rnp.spherical_to_cartesian( elon, elat, rate )
+            euler_pole_1 = rnp.spherical_to_cartesian( elon1, elat1, rate1 )
+            euler_pole_2 = rnp.spherical_to_cartesian( elon2, elat2, rate2 )
             for i,a in enumerate(age_list):
-                final_pole = rnp.rotate(initial_pole, euler_pole, rate*a)
+                if a < switch:
+                    final_pole = rnp.rotate(initial_pole, euler_pole_1, rate1*a)
+                else:
+                    mid_pole = rnp.rotate(initial_pole, euler_pole_1, rate1*switch)
+                    final_pole = rnp.rotate(mid_pole, euler_pole_2, rate2*(a-switch))
+
                 lon,lat,_ = rnp.cartesian_to_spherical(final_pole)
                 pathlons[i] = lon[0]
                 pathlats[i] = lat[0]
-            ax.plot(pathlons,pathlats,color='r', transform=ccrs.PlateCarree(), alpha=0.1)
+
+            ax.plot(pathlons,pathlats,color='b', transform=ccrs.PlateCarree(), alpha=0.1)
 
         for p in lon_lats:
             plot_pole(ax, p[0], p[1], a95=10.)
