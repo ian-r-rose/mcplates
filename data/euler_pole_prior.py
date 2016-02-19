e447e518b38d4ddea9373301ffc4a54b6359fcc8
Ian Rose <ian.r.rose@gmail.com>
2016-02-19 14:16:56 -0800

    Minor cleanup

--- data/euler_pole_prior.py
+++ data/euler_pole_prior.py
@@ -69,6 +69,7 @@ while i < n_samples:
     x = np.array([1.,1.,1.])
     while np.dot(x,x) > 1.:
         x = 2.*random.random(3)-1.
+    x = x/np.sqrt(np.dot(x,x))
     lon,lat,norm = cartesian_to_spherical( x )
     try:
         lon_index = np.floor(lon/dlon)
@@ -88,24 +89,18 @@ while i < n_samples:
     except KeyError:
         continue
 
-#ax = plt.axes( projection=ccrs.Mollweide(0.) )
-#c = ax.pcolormesh(lons,lats,vals, transform=ccrs.PlateCarree())
-#ax.scatter(lon_samples,lat_samples, c=val_samples, transform=ccrs.PlateCarree())
-#plt.colorbar(c)
-#plt.show()
-blah_x = np.linspace(-1., 1., 100)
-blah_y = np.ones_like(blah_x)*0.5
-beta_x = np.linspace(0.,1.,1000)
-beta_uniform = beta.pdf( beta_x, 1., 1.)
-beta_nonuniform = beta.pdf( beta_x, 3, 3)
+ax = plt.axes( projection=ccrs.Mollweide(0.) )
+c = ax.pcolormesh(lons,lats,vals, transform=ccrs.PlateCarree())
+ax.scatter(lon_samples,lat_samples, c=val_samples, transform=ccrs.PlateCarree())
+plt.colorbar(c)
+plt.show()
+
 uniform_x = np.linspace(0., np.pi, 100)
 uniform_y = np.sin(uniform_x)*0.5
-kappa = -2.5
+kappa = -1.0
 watson_x = np.linspace(0., np.pi, 100)
 watson_y = 1./sp.hyp1f1(0.5,1.5,kappa) * np.exp(kappa * np.cos(watson_x)**2.)*np.sin(watson_x)/2.
 plt.hist( val_samples, bins=10, normed=True )
-#plt.plot( uniform_x, uniform_y, 'r', lw=3)
-#plt.plot( 2.*beta_x-1, beta_uniform/2., 'r', lw=3)
-plt.plot( 2.*beta_x-1 + np.pi/2., beta_nonuniform/2., 'r', lw=3)
+plt.plot( uniform_x, uniform_y, 'r', lw=3)
 plt.plot( watson_x , watson_y, 'g', lw=3)
 plt.show()
